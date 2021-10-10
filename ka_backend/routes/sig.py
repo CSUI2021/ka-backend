87cbad48dfb5a88cc79a3ff8fd5e3b6716aa4304
Naufal W <naufalwwidyatama@gmail.com>
2021-10-10 18:27:18 +0700

    add sig & competition pagination

--- ka_backend/routes/sig.py
+++ ka_backend/routes/sig.py
@@ -1,11 +1,13 @@
 from typing import List
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 from ka_backend.models import SIG
 
 router = APIRouter(prefix="/sig")
 
 
 @router.get("/list", response_model=List[SIG])
-async def get_sig_list():
-    sig_list = await SIG.objects.all()
+async def get_sig_list(
+    page_num: int = Query(1, ge=1), page_size: int = Query(10, ge=1)
+):
+    sig_list = await SIG.objects.paginate(page_num, page_size).all()
     return sig_list
