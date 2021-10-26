769dd0b0597f0e8c86767b079a17aaa18091caaa
Rendy Arya Kemal <renrror@gmail.com>
2021-10-26 20:44:48 +0700

    Notify user to try again on AuthError

--- ka_backend/routes/auth.py
+++ ka_backend/routes/auth.py
@@ -7,7 +7,7 @@ from fastapi.responses import HTMLResponse, JSONResponse, RedirectResponse
 from ka_backend.helper.settings import settings
 from ka_backend.models import Student
 from ka_backend.plugins import manager
-from ka_backend.sso.client import UIClient
+from ka_backend.sso.client import AuthError, UIClient
 from ka_backend.sso.types import KDAttributes
 
 router = APIRouter(prefix="/auth")
@@ -23,37 +23,31 @@ def login():
 async def callback(ticket: str = Query(...)):
     try:
         sso_response = await client.authenticate(ticket)
+    except AuthError:
+        return {"err": "Authentication failure. Please try again."}
     except Exception:
         traceback.print_exc()
         return {"err": "An error has occured."}
 
     kd = sso_response["attributes"]["kd_attributes"]
-    kd = cast(KDAttributes, kd)
-    if kd["faculty"] != "ILMU KOMPUTER":
+    if kd and kd["faculty"] != "ILMU KOMPUTER":
         raise HTTPException(
             status_code=401,
             detail="This service is only available for Faculty of Computer Science students.",
         )
 
-    user = await Student.objects.get_or_none(npm=sso_response["attributes"]["npm"])
-    if not user:
-        jurusan_name = "_".join(kd["study_program"].split()[:2])
-        user = await Student.objects.create(
-            npm=sso_response["attributes"]["npm"],
-            nama=sso_response["attributes"]["nama"],
-            jurusan=jurusan_name.lower(),
-        )
+    kd = cast(KDAttributes, kd)
+    user = await Student.objects.get_or_create(npm=sso_response["attributes"]["npm"])
+    jurusan_name = "_".join(kd["study_program"].split()[:2])
+    await user.update(
+        nama=sso_response["attributes"]["nama"],
+        jurusan=jurusan_name.lower(),
+    )
 
     response = HTMLResponse(
         content="""<script>window.opener.postMessage("logged", "*")</script>"""
     )
-    token = manager.create_access_token(
-        data=dict(
-            sub=dict(
-                npm=user.npm,
-            )
-        )
-    )
+    token = manager.create_access_token(data=dict(sub=dict(npm=user.npm)))
     manager.set_cookie(response, token)
     return response
 
