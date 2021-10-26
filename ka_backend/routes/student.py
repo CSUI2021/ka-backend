cccc9b83da1974167aa729bfb21b32b571a6a0b9
Rendy Arya Kemal <renrror@gmail.com>
2021-10-26 19:45:03 +0700

    Cleanup code to be more clean

--- ka_backend/routes/student.py
+++ ka_backend/routes/student.py
@@ -21,25 +21,20 @@ async def list(
 ):
 
     students = Student.objects.select_related("house")
-
     if name:
         students = students.filter(nama__icontains=name)
-
     if major:
         students = students.filter(jurusan__exact=major)
-
     if house:
         students = students.filter(house__nama__exact=house)
 
     if sort == "asc":
         students = students.order_by("nama")
-
     elif sort == "desc":
         students = students.order_by("-nama")
 
-    students = await students.paginate(page=page, page_size=10).all()
-
-    return students
+    result = await students.paginate(page=page, page_size=10).all()
+    return result
 
 
 @router.get(
