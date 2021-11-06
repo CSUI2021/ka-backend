21e28c08e315aab8d3788ebe9ef4bad8dbacc699
Naufal W <naufalwwidyatama@gmail.com>
2021-11-06 22:28:17 +0700

    Add competition admin panel

--- ka_backend/routes/admin/__init__.py
+++ ka_backend/routes/admin/__init__.py
@@ -2,7 +2,9 @@ from fastapi import APIRouter
 
 from ka_backend.routes.admin.story import router as StoryRouter
 from ka_backend.routes.admin.sig import router as SIGRouter
+from ka_backend.routes.admin.competition import router as CompetitionRouter
 
 router = APIRouter(prefix="/admin", tags=["SIG"])
 router.include_router(StoryRouter)
 router.include_router(SIGRouter)
+router.include_router(CompetitionRouter)
