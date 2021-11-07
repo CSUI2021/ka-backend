7e39a4196ecf4d18f36af7e0176620473318bfb2
Rendy Arya Kemal <renrror@gmail.com>
2021-11-07 16:11:40 +0700

    Remove admin routes from schema

--- ka_backend/routes/admin/__init__.py
+++ ka_backend/routes/admin/__init__.py
@@ -5,7 +5,7 @@ from ka_backend.routes.admin.sig import router as SIGRouter
 from ka_backend.routes.admin.story import router as StoryRouter
 from ka_backend.routes.admin.student import router as StudentRouter
 
-router = APIRouter(prefix="/admin", tags=["SIG"])
+router = APIRouter(prefix="/admin", include_in_schema=False)
 router.include_router(StoryRouter)
 router.include_router(SIGRouter)
 router.include_router(CompetitionRouter)
