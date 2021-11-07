0f8af73a4bbd221f0607193de5fa0ee9dbb55d47
Naufal W <naufalwwidyatama@gmail.com>
2021-11-07 09:28:05 +0700

    Order module alphabetically

--- ka_backend/routes/admin/__init__.py
+++ ka_backend/routes/admin/__init__.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 
-from ka_backend.routes.admin.story import router as StoryRouter
-from ka_backend.routes.admin.sig import router as SIGRouter
 from ka_backend.routes.admin.competition import router as CompetitionRouter
+from ka_backend.routes.admin.sig import router as SIGRouter
+from ka_backend.routes.admin.story import router as StoryRouter
 
 router = APIRouter(prefix="/admin", tags=["SIG"])
 router.include_router(StoryRouter)
